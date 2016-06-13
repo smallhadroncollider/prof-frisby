99c65b1821fceb8a67a578ea5b0f3ffc9707b37c
Mark Wales <mark@smallhadroncollider.com>
2016-06-13 13:38:50 +0100

    added maybe helper function

--- functors/Maybe.js
+++ functors/Maybe.js
@@ -1,7 +1,11 @@
+import { curry } from "ramda";
+
 /**
  * Maybe functor
  * Will only call map if value is set
  **/
+const guard = Symbol("maybe guard");
+
 const Maybe = value => {
     let self = {};
     
@@ -10,10 +14,25 @@ const Maybe = value => {
 
     // map:: (a -> b) -> Maybe a -> Maybe b
     self.map = f => self.isNothing() ? Maybe.of(null) : Maybe.of(f(value));
+    
+    // value :: Symbol -> a
+    self.value = symbol => {
+        if (symbol !== guard) {
+            throw new Error("Maybe.value() cannot be accessed directly. Use maybe() helper");
+        }
+
+        return value;
+    }
 
     return self;
 };
 
 Maybe.of = value => Maybe(value);
 
-export default Maybe;
+// maybe :: b -> (a -> b) -> Maybe a -> b 
+const maybe = curry((x, f, m) => m.isNothing() ? x : f(m.value(guard)));
+
+export {
+    Maybe as default,
+    maybe
+};
